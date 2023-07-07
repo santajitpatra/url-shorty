3697e41b53beddfdaead38a95093acc394d2c887
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-07 23:43:17 +0530

    feat : server side rendering
    server side rendering
    EJS model

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const urlRoute = require("./routes/url");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const URL = require("./models/url");
+
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 
 const app = express();
 const port = 8000;
@@ -10,11 +13,18 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short_url").then(
   console.log("mongodb://127.0.0.1:27017/short_url successfully connected")
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async (req, res) => {
+
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
@@ -28,7 +38,11 @@ app.get("/:shortId", async (req, res) => {
       },
     }
   );
+  // if (entry) {
   res.redirect(entry.redirectURL);
+  // } else {
+  //   res.status(404).send("Short URL not found");
+  // }
 });
 
 app.listen(port, () => {
