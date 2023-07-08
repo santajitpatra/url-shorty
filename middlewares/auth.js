f52ace8264111eb541458339c834bdc0e4e9561d
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-08 13:04:58 +0530

    feat : header Authorization
    Authorization by using the header  token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,13 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
+  // const userUid = req.cookies?.uid;
+  const userUid = req.headers["Authorization"];
 
   if (!userUid) return res.redirect("/login");
-  const user = getUser(userUid);
+  const token = userUid.split("Bearer ")[1];
+  // const user = getUser(userUid);
+  const user = getUser(token);
 
   if (!user) return res.redirect("/login");
 
@@ -13,9 +16,13 @@ async function restrictToLoggedinUserOnly(req, res, next) {
 }
 
 async function checkAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
+  // const userUid = req.cookies?.uid;
+    const userUid = req.headers["authorization"];
+    const token = userUid.split("Bearer ")[1];
 
-  const user = getUser(userUid);
+
+  // const user = getUser(userUid);
+  const user = getUser(token);
 
   req.user = user;
   next();
--- service/auth.js
+++ service/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const secret = "secret";
+const secret = "secret-to-hogan-secret";
 
 function setUser(user) {
   return jwt.sign(
